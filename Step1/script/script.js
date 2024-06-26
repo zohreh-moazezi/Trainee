68a66365b4a5095d2772cb10f3b9165f40d2f848
zohreh moazezi <107259983+zohreh-moazezi@users.noreply.github.com>
2024-06-26 14:47:01 +0430

    Add login request handling to script.js
    - Added logic to send a POST request to the login endpoint using Axios
    - Access to baseURL by "client.defaults"
    - Stored refresh and access token in localStorage upon successful login
    -Displayed a success alert message
    -Handled error cases by logging the error and highlighting input fields for a 401 status

--- Step1/script/script.js
+++ Step1/script/script.js
@@ -1,43 +1,60 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("login-form");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
+const axios = window.axios;
+console.log(axios);
+const client = axios.create({
+  baseURL: "http://localhost:4200/",
+});
 
-    const userName = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+const form = document.getElementById("login-form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    const useNameError = document.getElementById("usernameError");
-    const passwordError = document.getElementById("passwordError");
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    const userNameInput = document.getElementById("username");
-    const passwordInput = document.getElementById("password");
+  const useNameError = document.getElementById("usernameError");
+  const passwordError = document.getElementById("passwordError");
 
-    useNameError.textContent = " ";
-    password.textContent = " ";
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
 
-    let valid = true;
+  useNameError.textContent = " ";
+  passwordError.textContent = " ";
 
-    if (!userName) {
-      useNameError.textContent = "Enter username";
-      userNameInput.classList.add("error");
-      valid = false;
+  let valid = true;
+
+  if (!username) {
+    useNameError.textContent = "Enter username";
+    usernameInput.classList.add("error");
+    valid = false;
+  }
+  if (!password) {
+    passwordError.textContent = "Enter password";
+    passwordInput.classList.add("error");
+    valid = false;
+  }
+
+  if (valid) {
+    try {
+      const response = await client.post(
+        `${client.defaults.baseURL}auth/login`,
+        {
+          username: username,
+          password: password,
+        }
+      );
+      console.log(response);
+      const { access_token, refresh_token } = response.data;
+      console.log(access_token);
+      console.log(refresh_token);
+      localStorage.setItem("access token:", access_token);
+      localStorage.setItem("refresh token:", refresh_token);
+      alert("Login Successful");
+    } catch (error) {
+      console.error(error);
+      if (error.response && error.response.status === 401) {
+        usernameInput.classList.add("error");
+        passwordInput.classList.add("error");
+      }
     }
-    if (!password) {
-      passwordError.textContent = "Enter password";
-      passwordInput.classList.add("error");
-      valid = false;
-    }
-
-    if (valid) {
-      form.submit();
-    }
-  });
-  userNameInput.addEventListener("input", () => {
-    userNameInput.classList.remove("error");
-    useNameError.textContent = "";
-  });
-  passwordInput.addEventListener("input", () => {
-    passwordError.textContent = "";
-    passwordInput.classList.remove("error");
-  });
+  }
 });
